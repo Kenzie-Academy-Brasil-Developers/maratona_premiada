bc0ceb7a63c024b62a7e372a4ca505f0dc271ea1
luizpcardoso <luizpauloreiscardosoo@gmail.com>
2022-06-11 14:27:41 -0300

    ajuste database

---
+++ assets/script/database.js
@@ -0,0 +1,51 @@
+const products = [
+  {
+    id: "01",
+    title: "Kenzie Burgão",
+    description: "O Hambúrguer do Dev Full Stack Kenzie!",
+    category: "Panificadora",
+    price: "28.00",
+    img: "../img/kenzie_burgao.png",
+  },
+  {
+    id: "02",
+    title: "Refrigerante",
+    description: "Refrigerante bem gelado para acompanhar sua refeição",
+    category: "Frutas",
+    price: "7.50",
+    img: "../img/refrigerante.png",
+  },
+  {
+    id: "03",
+    title: "Kenzie Burguinho",
+    description:
+      "Delicioso hambúrguer acompanhado de um ovo frito de gema mole!",
+    category: "Panificadora",
+    price: "22.00",
+    img: "../img/kenzie_burguinho.png",
+  },
+  {
+    id: "04",
+    title: "Frango Frito",
+    description: "Frango frito e crocante com molho especial",
+    category: "Panificadora",
+    price: "25.00",
+    img: "../img/frango_frito.png",
+  },
+  {
+    id: "05",
+    title: "Batata Frita",
+    description: "Batata frita acompanhada de molho especial",
+    category: "Panificadora",
+    price: "15.00",
+    img: "../img/batata_frita.png",
+  },
+  {
+    id: "06",
+    title: "Sanduíche com Frango Frito",
+    description: "Pão, frango frito e salada para acompanhar",
+    category: "Panificadora",
+    price: "20.00",
+    img: "../img/sanduiche_frango_frito.png",
+  },
+];
